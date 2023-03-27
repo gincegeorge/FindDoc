6faf5aad598d785c24f3790f0aef012e10c51201
Gince George <gincegeo@gmail.com>
2023-03-27 12:08:23 +0530

    login feature

--- backend/Controllers/AuthControllers.js
+++ backend/Controllers/AuthControllers.js
@@ -9,18 +9,33 @@ const createToken = (id) => {
     })
 }
 
-// const handleErrors = (err) => {
-//     let errors = { name: "", email: "", phone: "", password: "" }
+const handleErrors = (err) => {
 
-//     if (err.code === 11000) {
-//         errors.email = "This email is already used"
-//         return errors
-//     }
+    let errors = { name: "", email: "", phone: "", password: "" }
 
-//     if(err.message.includes('Users validation failed')){
+    if (err.code === 11000) {
 
-//     }
-// }
+        const keys = Object.keys(err.keyValue)
+
+        if (keys.includes('email')) {
+            errors.email = "This email is already used!"
+        } else if (keys.includes('phone')) {
+            errors.phone = 'This phone number is already used!'
+        }
+
+        return errors
+    }
+
+    if (err.message.includes('Incorrect email')) {
+        errors.email = "Incorrect email"
+    } else if (err.message.includes('Incorrect password')) {
+        errors.password = "Incorrect password"
+    }
+
+    return errors
+
+
+}
 
 module.exports.register = async (req, res, next) => {
     try {
@@ -30,18 +45,50 @@ module.exports.register = async (req, res, next) => {
 
         const token = createToken((await user)._id)
 
-        res.cookie('jwt', token, {
+        res.cookie('newCookie', token, {
             withCredentials: true,
             httpOnly: true,
-            maxAge: maxAge * 1000
+            maxAge: maxAge * 1000,
+            sameSite: 'none', secure: true
         })
         res.json({ user: (await user)._id, created: true })
 
     } catch (err) {
-        console.log(err);
-        res.status(404).send('failed')
+        const errors = handleErrors(err)
+
+        console.log(errors);
+
+        res.json({ errors, created: false, status: 409 })
     }
 };
 
-module.exports.login = async (req, res, next) => { };
+module.exports.login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+
+        const user = userModel.login(email, password)
+
+        const token = createToken((await user)._id)
+
+        // res.cookie('jwt', token, {
+        //     withCredentials: true,
+        //     httpOnly: true,
+        //     maxAge: maxAge * 1000,
+        //     sameSite: 'none', secure: true
+        // })
+        res.status(200).json(
+            {
+                user: (await user)._id,
+                created: true,
+                token: "jwt" + token
+            })
+
+    } catch (err) {
+        const errors = handleErrors(err)
+
+        console.log(errors);
+
+        res.json({ errors, created: false, status: 409 })
+    }
+};
 
--- backend/Routes/AuthRoutes.js
+++ backend/Routes/AuthRoutes.js
@@ -3,6 +3,6 @@ const { register,login } = require("../Controllers/AuthControllers");
 
 router.get("/");
 router.post("/register", register);
-// router.post("/login", login);
+router.post("/login", login);
 
 module.exports = router; 
